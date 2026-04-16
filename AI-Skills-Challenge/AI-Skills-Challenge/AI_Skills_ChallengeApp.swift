33d0cadea20d9c8fa9101845db27bec5251fc6c0
Roger Pingleton <rogermp8@gmail.com>
2026-04-15 20:37:24 -0400

    add xcode project, artwork, project document

---
+++ AI-Skills-Challenge/AI-Skills-Challenge/AI_Skills_ChallengeApp.swift
@@ -0,0 +1,32 @@
+//
+//  AI_Skills_ChallengeApp.swift
+//  AI-Skills-Challenge
+//
+//  Created by Jenny Wilder on 4/15/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AI_Skills_ChallengeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
